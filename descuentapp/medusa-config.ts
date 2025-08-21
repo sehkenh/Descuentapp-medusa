8c7a38893f152236c4b347c2d9962b5221d2b41e
sehkenh <gonzalo.alvarez.dev@gmail.com>
2025-08-21 15:45:31 -0400

    Update 21-08-2025

--- descuentapp/medusa-config.ts
+++ descuentapp/medusa-config.ts
@@ -12,5 +12,9 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
-  }
+  },
+  modules: [{
+    resolve: "./src/modules/brand", 
+  },
+ ],
 })
---
+++ descuentapp/src/modules/brand/models/brand.ts
@@ -0,0 +1,7 @@
+import { Utils } from "@mikro-orm/core";
+import { model } from "@medusajs/framework/utils"
+
+export const Brand = model.define("brand", {
+    id: model.id().primaryKey(),
+    name: model.text(),
+})
